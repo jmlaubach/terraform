312460a2779ac1309fd61215601ed26485a6b5b4
jmlaubach <joshm.laubach@gmail.com>
2022-08-16 11:14:31 -0400

    Add backend configuration for state file with AWS S3 and DynamoDB

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-    region= "us-east-1"
+    region  = "us-east-1"
     profile = "default"
     shared_credentials_file = "/Users/jlaubach/.aws/credentials"
 }
@@ -8,6 +8,42 @@ provider "aws" {
 
 variable "webserver-ami" {
     type = string
+}
+
+# # Create S3 bucket to host terraform state file
+
+resource "aws_s3_bucket" "state-bucket" {
+    bucket = "jlaubach-terraform-state"
+    versioning {
+      enabled = true
+    }
+    server_side_encryption_configuration {
+      rule {
+        apply_server_side_encryption_by_default {
+          sse_algorithm = "AES256"
+        }
+      }
+    }
+}
+
+resource "aws_dynamodb_table" "state-lock" {
+    name         = "jlaubach-terraform-lock"
+    billing_mode = "PAY_PER_REQUEST"
+    hash_key     = "LockID"
+    attribute {
+      name = "LockID"
+      type = "S"
+    } 
+}
+
+terraform {
+    backend "s3" {
+      bucket         = "jlaubach-terraform-state"
+      key            = "global/s3/terraform.tfstate"
+      region         = "us-east-1"
+      dynamodb_table = "jlaubach-terraform-lock"
+      encrypt        = true
+    }
 }
 
 # # Create launch configuration
