e32916f746cdde503354234238c8e450d1fac37f
joshua.laubach <josh.laubach@musictoday.com>
2022-08-17 11:10:21 -0400

    Separate backend component creation.

--- ecs/backend.tf
+++ ecs/backend.tf
@@ -1,28 +1,4 @@
-# # Create S3 bucket to host terraform state file
-
-resource "aws_s3_bucket" "state-bucket" {
-    bucket = "jlaubach-terraform-state-ecs"
-    versioning {
-      enabled = true
-    }
-    server_side_encryption_configuration {
-      rule {
-        apply_server_side_encryption_by_default {
-          sse_algorithm = "AES256"
-        }
-      }
-    }
-}
-
-resource "aws_dynamodb_table" "state-lock" {
-    name         = "jlaubach-terraform-lock-ecs"
-    billing_mode = "PAY_PER_REQUEST"
-    hash_key     = "LockID"
-    attribute {
-      name = "LockID"
-      type = "S"
-    } 
-}
+# # Set up the backend for ECS tasks
 
 terraform {
     backend "s3" {
