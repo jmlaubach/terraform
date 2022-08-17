22eeb8cce77b0c1ea0ab8d5d9cee11640d2a6273
joshua.laubach <josh.laubach@musictoday.com>
2022-08-17 09:09:10 -0400

    Separate provider section into separate file.

---
+++ ecs/provider.tf
@@ -0,0 +1,6 @@
+provider "aws" {
+    region  = "us-east-1"
+    profile = "default"
+#    shared_credentials_file = "/Users/jmlst/.aws/credentials"
+    shared_credentials_file = "/Users/jlaubach/.aws/credentials"
+}
